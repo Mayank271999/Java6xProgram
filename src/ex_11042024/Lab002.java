b590cc79ce781e1bc08792c4b361cf972b304e44
Mayank271999 <mayankdeopamd29@gmail.com>
2024-04-20 08:03:29 +0530

    First commit

---
+++ src/ex_11042024/Lab002.java
@@ -0,0 +1,8 @@
+package ex_11042024;
+
+public class Lab002 {
+    // keywords --> reserved words , pre defined words , here keywords are public ,class etc
+    public static void main(String[] args) {
+        System.out.println("Hi");
+    }
+}
---
+++ src/ex_11042024/Lab003.java
@@ -0,0 +1,16 @@
+package ex_11042024;
+
+public class Lab003 {
+    public static void main(String[] args) {
+        //variable
+
+        int age = 65;
+        // Integer type of container storing 65 s value in the variable name = age
+        age = 70;
+        age = -1;
+        age = age +2;
+
+
+        System.out.println(age);
+    }
+}
