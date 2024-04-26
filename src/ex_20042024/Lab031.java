4d547d41be80993f1e96dfb52bfdbc579faf3723
Mayank271999 <mayankdeopamd29@gmail.com>
2024-04-26 20:34:09 +0530

    String class

---
+++ src/ex_20042024/Lab031.java
@@ -0,0 +1,10 @@
+package ex_20042024;
+
+public class Lab031 {
+
+    public static void main(String[] args) {
+        // Strings --> bunch of characters , it is a class
+        String name = "mayank";
+        System.out.println("your name is "+ name);
+    }
+}
---
+++ src/ex_20042024/Lab032.java
@@ -0,0 +1,40 @@
+package ex_20042024;
+
+public class Lab032 {
+    public static void main(String[] args) {
+
+        //String name = " mayank " ; //  ASSIGNMENT OPERQATOR
+
+      //  String name2 = new String("MAYANK" ); // New Operator  two ways to create string
+
+        // Both of them , save the values in dif. ways JVM
+
+          // name.length , name.toLowerCase
+
+
+        String s1 = " mayank ";
+        String s2 = s1;
+        System.out.println(s2);
+
+        String s3 = "TTA";
+        s3.toLowerCase();   // Not assigned
+        System.out.println(s3);
+
+        String s4 = "TTA";
+        s4=  s4.toLowerCase(); // assigned
+        System.out.println(s4);
+
+        String name = " hello";
+        String name1 = " hello"; // only one string created because it is already present "hello";
+        System.out.println();
+
+
+
+
+
+
+
+
+
+    }
+}
